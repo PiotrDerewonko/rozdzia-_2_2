0c37929dcecdd01d2e367db57c9de4041344fac9
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-02-11 17:49:14 +0100

    dodanie modeli do Panelu admina

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from posts.models import Author, post
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['nick', 'email', 'bio']
+    list_filter = ['email']
+    search_fields = ['nick']
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'created', 'modified', 'author_id']
+    list_filter = ['title']
+    search_fields = ['title', 'content']
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(post, postAdmin)
