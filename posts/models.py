1ef5f791272cfdadeb6ab918724e540b56091d9c
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-02-11 16:37:35 +0100

    drobne poprawki

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    nick = models.CharField(max_length=50)
+    email = models.EmailField()
+    bio = models.TextField(null=True)
+
+class post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        'posts.Author',
+        on_delete=models.CASCADE
+    )
+
+
