15de9e2c272cb615b44582a1e974800406df9659
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-02-17 18:50:51 +0100

    uproszczenie adresow url

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from .views import post_list, post_detail, authors_list, author_detail, post_add
 app_name = 'posts'
 
 urlpatterns = [
-    path('posts_list/', post_list, name='lista_postow'),
+    path('', post_list, name='lista_postow'),
     path('post/<int:id>', post_detail, name='post'),
     path('post_add/', post_add, name='post_add'),
     path('authors_list/', authors_list, name='lista_autorow'),
